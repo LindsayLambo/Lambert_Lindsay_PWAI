129981af9fa6f4b4e11dd1c80b1e8bf36492d906
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-20 18:04:55 -0400

    First part of the duel 1
    I think this is correct…hopefully…

---
+++ Wk1/duel1.js
@@ -0,0 +1,16 @@
+// Lindsay Lambert May 20, 2014 The Duel Part I
+
+(function(){
+    console.log("FIIIIIIIGHT!!!");
+    //names of fighters
+    var fighter1Name="Ugly Dude";
+    var fighter2Name="Uglier Dude";
+    //damages to fighters
+    var fighter1Damage=20;
+    var fighter2Damage=20;
+    //beginning health is 100 for fighters
+    var fighter1Health=100;
+    var fighter2Health=100;
+    //initiating the round
+    var round=0;
+    
