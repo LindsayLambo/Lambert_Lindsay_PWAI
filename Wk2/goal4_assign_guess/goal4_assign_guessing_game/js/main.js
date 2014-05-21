6c64978d7dfdb2f5df3bd06e90eaa377ad87023c
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-21 13:27:06 -0400

    Guessing Game
    Starting with defining variables and DOM elements…

--- Wk2/goal4_assign_guess/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guess/goal4_assign_guessing_game/js/main.js
@@ -9,6 +9,26 @@
 
 //Game variables
 (function ()
+{
+	var playerGuess=0;
+	var guessesLeft=3;
+	var guessesSoFar=0;
+//DOM elements
+var DOM={
+	input:document.querySelector("#input"),
+	output:document.querySelector("#output"),
+	button:document.querySelector("button")
+};	
+//Letting computer pick a random number
 
+
+
+
+
+
+
+
+
+}
 
 })();
