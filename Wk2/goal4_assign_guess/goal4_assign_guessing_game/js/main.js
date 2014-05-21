666b739e8f74609c6e127d8dd9a409257535f00b
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-21 13:39:46 -0400

    Guessing Game
    Added math to choose random number for the magic number and validating
    the players’ clicks/input now…

--- Wk2/goal4_assign_guess/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guess/goal4_assign_guessing_game/js/main.js
@@ -20,6 +20,16 @@ var DOM={
 	button:document.querySelector("button")
 };	
 //Letting computer pick a random number
+var magicNumber=Math.floor((Math.random()*10)+1);
+//Using a function to respond to the users' clicks with console log
+var clickFunction=function(e){
+	console.log(guessesLeft);
+	validatePlayerInput();
+};
+//Since the the guesses, or users' input, are clicks, we have to validate and call the timeToPlay() function
+var validatePlayerInput=function(){
+	playerGuess
+}
 
 
 
